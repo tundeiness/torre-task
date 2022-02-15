25bb8a1fc92e9c6491c227e058f8b3b63d6c562b
tundeiness <tundeiness@yahoo.co.uk>
2022-02-15 15:33:33 +0100

    create search page

---
+++ src/Components/searchPage.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const searchPage = () => {
+  const dud = '';
+  return (
+    <div>searchPage</div>
+  );
+};
+
+export default searchPage;
