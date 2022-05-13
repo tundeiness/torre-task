58600738c77044f3130a2616fa328d8b2423a43f
tundeiness <tundeiness@yahoo.co.uk>
2022-05-13 14:38:38 +0100

    refactor sidebar and icons

--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -4,8 +4,9 @@ import { BsSearch } from 'react-icons/bs';
 import {
   IoCompassOutline,
   IoMegaphoneOutline,
-  IoMdBriefcase,
-} from 'react-icons/io';
+} from 'react-icons/io5';
+
+import { IoMdBriefcase } from 'react-icons/io';
 
 import { GoSettings } from 'react-icons/go';
 import { TiMessages } from 'react-icons/ti';
@@ -19,31 +20,40 @@ const sidebar = () => {
   const unlow = 0;
   // const [toggleMenu, setToggleMenu] = useState(toggl);
   // console.log(toggl);
+
   return (
     <>
-      <ul className="d-flex flex-column border border-danger sidebar">
+      <ul className="d-flex flex-column sidebar">
         <li className="sidebar__brand-wrapper">
           <a className="sidebar-brand" href="./">
             torre
           </a>
         </li>
         {[
-          'Search',
-          'jobs/gigs',
-          'Post a job',
-          'Your jobs(posted or applied)',
-          'Preferences',
-          'Your genome',
-          'Signals',
-        ].map((itms, _indx) => (
-          <Item key={itms.id} title={itms} />
+          ['Search', <BsSearch key="0" className="bssearch" />],
+          ['jobs/gigs', <IoCompassOutline key="1" className="job" />],
+          ['Post a job', <IoMegaphoneOutline key="2" className="post" />],
+          ['Your jobs(posted or applied)', <IoMdBriefcase key="3" className="applied" />],
+          ['Preferences', <GoSettings key="4" className="preference" />],
+          ['Your genome', <TiMessages key="5" className="genome" />],
+          ['Signals', <GiRoad key="6" className="signal" />],
+        ].map((itms, indx) => (
+          <span className="d-flex flex-row ms-3 pt-4" key={itms.id}>
+            <span className="d-flex flex-column justify-content-center">{itms[1]}</span>
+            <Item key={itms.id} title={itms[0]} />
+          </span>
         ))}
         <hr />
-        {['Messages', 'Torres Product roadmap', 'Request features'].map(
-          (itms, _indx) => (
-            <Item key={itms.id} title={itms} />
-          ),
-        )}
+        {[
+          ['Messages', <GrLanguage key="0" className="grlanguage" />],
+          ['Torres Product roadmap', <BiCode key="1" className="product" />],
+          ['Request features', <AiOutlineBulb key="2" className="request" />],
+        ].map((itms, _indx) => (
+          <span className="d-flex flex-row ms-3 pt-4" key={itms.id}>
+            <span className="d-flex flex-column justify-content-center">{itms[1]}</span>
+            <Item key={itms.id} title={itms[0]} />
+          </span>
+        ))}
         <hr />
       </ul>
     </>
